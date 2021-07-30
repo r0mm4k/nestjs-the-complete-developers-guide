22dc00992ccf076d716088461454e5b52a676710
Roma Makarov <r0mmm4k@gmail.com>
2021-07-30 17:02:24 +0300

    - add dto messages

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
@@ -11,7 +12,7 @@ export class MessagesController {
   }
 
   @Post()
-  create(@Body() body: any) {
+  create(@Body() body: CreateMessageDto) {
     console.log(body);
   }
 }
