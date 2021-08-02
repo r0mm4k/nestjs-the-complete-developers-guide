73db62e49115f0d77a0be7c6b600ada5c1e2b96f
Roma Makarov <r0mmm4k@gmail.com>
2021-08-02 18:43:26 +0300

    - Removing Records

--- my-car-value/src/users/users.service.ts
+++ my-car-value/src/users/users.service.ts
@@ -33,5 +33,13 @@ export class UsersService {
     return this.repo.save(user);
   }
 
-  remove() {}
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    if (!user) {
+      throw new Error('user not found');
+    }
+
+    return this.repo.remove(user);
+  }
 }
