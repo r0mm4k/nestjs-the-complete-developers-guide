13c236147d6b351dadeee7d5cae081980a8f3431
Roma Makarov <r0mmm4k@gmail.com>
2021-08-06 18:07:51 +0300

    - A Bit of Type Safety Around Serialize

--- my-car-value/src/interceptors/serialize.interceptor.ts
+++ my-car-value/src/interceptors/serialize.interceptor.ts
@@ -5,14 +5,14 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { map, Observable } from 'rxjs';
-import { plainToClass } from 'class-transformer';
+import { ClassConstructor, plainToClass } from 'class-transformer';
 
-export function Serialize(dto: any) {
+export function Serialize(dto: ClassConstructor<any>) {
   return UseInterceptors(new SerializeInterceptor(dto));
 }
 
 export class SerializeInterceptor implements NestInterceptor {
-  constructor(private dto: any) {}
+  constructor(private dto: ClassConstructor<any>) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
