be8b110bbca8a479d698840162caf1b0ba157fa0
Roma Makarov <r0mmm4k@gmail.com>
2021-08-08 22:01:31 +0300

    - Setting up Sessions

--- my-car-value/src/main.ts
+++ my-car-value/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({ keys: ['akd1asdSsa12c'] }));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
